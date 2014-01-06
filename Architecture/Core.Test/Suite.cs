818cbd4081f725464dce2f45e766d83a1663cb6c
SMoni <steffen.monarth@ingenieur.de>
2014-01-06 19:32:21 +0100

    Starting Card

---
+++ Architecture/Core.Test/Suite.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using NUnit.Framework;
+
+namespace Architecture.Core.Test {
+    [TestFixture]
+    class Suite {
+
+        [Test]
+        public void PropertyChangeTriggersLastUpdateChange()
+        {
+            var card = new Card();
+
+            Assert.AreEqual(card.Created, card.LastUpdated);
+
+            Thread.Sleep(1000);
+
+            card.Description = "Hello World";
+
+            Assert.AreNotEqual(card.Created, card.LastUpdated);
+        }
+
+    }
+}
