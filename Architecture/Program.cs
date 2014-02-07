f86a6b7da7a3fcb4ca89fd1744ac93e45ece8fff
SMoni <steffen.monarth@ingenieur.de>
2014-02-07 18:46:40 +0100

    Starting Facade

--- Architecture/Program.cs
+++ Architecture/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Architecture.Tools;
+using Architecture.Core;
 
 namespace Architecture {
     class Program {
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
+            var facade = new Facade();
+
+            var loadedBoard = facade.Load<Board>(Guid.NewGuid());
+
+            loadedBoard.Save();
+            loadedBoard.Delete();
+
+            var createdBoard = facade.Create<Board>();
+
+            createdBoard.Save();
+
+            var createdSection = facade.Create<Section>();
+
+            createdSection.Name = "Hello World";
+
+            createdBoard.Add(createdSection);
+
+            var createdCardNo1 = facade.Create<Card>();
+
+            createdCardNo1.Description = "Do something";
+            createdCardNo1.Responsible = "Me";
+            createdCardNo1.Requester   = "You";
+
+            createdSection.Add(createdCardNo1);
+
+            var createdCardNo2 = facade.Create<Card>();
+
+            createdCardNo2.Description = "Do something else...";
+            createdCardNo2.Responsible = "You";
+            createdCardNo2.Requester   = "Me";
+
+            createdSection.Add(createdCardNo2);
+
+            createdBoard.Delete();
+
         }
     }
 }
---
+++ Architecture/Tools/Facade.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Architecture.Tools {
+    public class Facade
+    {
+        public T Create<T>()
+        {
+            return default(T);
+        }
+
+        public T Load<T>(Guid id)
+        {
+            return default(T);
+        }
+    }
+
+    internal static class ExtensionMethods
+    {
+        public static void Save<T>(this T toSave)
+        {
+
+        }
+
+        public static void Delete<T>(this T toDelete)
+        {
+
+        }
+    }
+
+}
